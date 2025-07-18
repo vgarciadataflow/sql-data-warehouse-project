f697657e32f9d4a61609a3513918d7e727b871c1
vgarciadataflow <vgarcia@dataflowsolutions.co>
2025-07-18 14:21:00 -0500

    Update cleanse_and_insert_sales_details.sql

--- scripts/cleanse_and_insert_sales_details.sql
+++ scripts/cleanse_and_insert_sales_details.sql
@@ -11,10 +11,17 @@ END AS sls_ship_dt,
 CASE WHEN sls_due_dt = 0 or LEN(sls_due_dt) != 8 THEN NULL
 	ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
 END AS sls_due_dt,
-sls_sales,
+CASE WHEN sls_sales<=0 or sls_sales IS NULL OR sls_sales!= sls_quantity * abs(sls_price) 
+	THEN sls_quantity * abs(sls_price)
+	ELSE sls_sales
+END AS sls_sales,
 sls_quantity,
-sls_price
-FROM bronze.crm_sales_details;
+CASE WHEN sls_price <= 0 or sls_price IS NULL 
+	THEN sls_sales / NULLIF(sls_quantity,0)
+	ELSE sls_price
+END sls_price
+FROM bronze.crm_sales_details
+
 
 -- Checking for duplicates
 
@@ -72,3 +79,61 @@ WHERE sls_sales != (sls_price * sls_quantity)
 ---1. If Sales is negative, zero, or null, derive it using quantity and price
 ---2. If price is zero or null, calculate is using sales and quantity
 ---3. If price is negative, convert it to a positive value
+
+
+--Insert into silver layer
+IF OBJECT_ID ('silver.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE silver.crm_sales_details;
+CREATE TABLE silver.crm_sales_details (
+	sls_ord_num		NVARCHAR(50),
+	sls_prd_key		NVARCHAR(50),
+	sls_cust_id		INT,
+	sls_order_dt	DATE,
+	sls_ship_dt		DATE,
+	sls_due_dt		DATE,
+	sls_sales		INT,
+	sls_quantity	INT,
+	sls_price		INT,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+
+
+INSERT INTO silver.crm_sales_details(
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+)
+SELECT 
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE WHEN sls_order_dt = 0 or LEN(sls_order_dt) != 8 THEN NULL
+	ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+END AS sls_order_dt,
+CASE WHEN sls_ship_dt = 0 or LEN(sls_ship_dt) != 8 THEN NULL
+	ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+END AS sls_ship_dt,
+CASE WHEN sls_due_dt = 0 or LEN(sls_due_dt) != 8 THEN NULL
+	ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+END AS sls_due_dt,
+CASE WHEN sls_sales<=0 or sls_sales IS NULL OR sls_sales!= sls_quantity * abs(sls_price) 
+	THEN sls_quantity * abs(sls_price)
+	ELSE sls_sales
+END AS sls_sales,
+sls_quantity,
+CASE WHEN sls_price <= 0 or sls_price IS NULL 
+	THEN sls_sales / NULLIF(sls_quantity,0)
+	ELSE sls_price
+END sls_price
+FROM bronze.crm_sales_details
+
+
+--Quality check the silver layer by rechecking some of the transformations from above
+--like duplicates, nulls, invalid dates, etc.
