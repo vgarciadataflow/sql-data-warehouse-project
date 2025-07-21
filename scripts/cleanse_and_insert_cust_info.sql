f9e53c82553cfa20843b6516e2b42c110a6f4a7d
vgarciadataflow <vgarcia@dataflowsolutions.co>
2025-07-21 16:06:51 -0500

    Update cleanse_and_insert_cust_info.sql

--- scripts/cleanse_and_insert_cust_info.sql
+++ scripts/cleanse_and_insert_cust_info.sql
@@ -1,3 +1,22 @@
+/*
+===================================================================
+Quality Checks
+===================================================================
+Script Purpose:
+   This script performs various quality checks for data consistency, 
+   accuracy, and standardization across the 'silver' schema. It includes
+   checks for:
+    -Null or duplicate primary keys.
+    -Unwanted spaces in string fields.
+    -Data standardization and consistency.
+    -Invalid date ranges and orders.
+    -Data consistency between related fields.
+
+Usage Notes:
+    -Run these checks 
+
+
+
 --Check for primary key duplicates and/or null values
 --Expectation: No results
 
