7d1fea93f26d24aabd95bac53c62ea1253e52e94
vgarciadataflow <vgarcia@dataflowsolutions.co>
2025-07-21 16:09:12 -0500

    Update cleanse_and_insert_erp_cust.sql

--- scripts/cleanse_and_insert_erp_cust.sql
+++ scripts/cleanse_and_insert_erp_cust.sql
@@ -1,3 +1,22 @@
+/*
+===================================================================
+Quality Checks
+===================================================================
+Script Purpose:
+   This script performs various quality checks for data consistency, 
+   accuracy, and standardization across the 'silver' schema. It includes
+   checks for:
+    -Null or duplicate primary keys.
+    -Unwanted spaces in string fields.
+    -Data standardization and consistency.
+    -Invalid date ranges and orders.
+    -Data consistency between related fields.
+
+Usage Notes:
+    -Run these checks after loading data into Silver layer
+    -Investigate and resolve any discrepancies found during the checks.
+=====
+
 SELECT
 CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, len(cid))
 	ELSE cid
