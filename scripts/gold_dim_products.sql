6b7a30e1246e8f4c6837035ff4f325078edbd068
vgarciadataflow <vgarcia@dataflowsolutions.co>
2025-07-22 16:15:10 -0500

    Update gold_dim_products.sql

--- scripts/gold_dim_products.sql
+++ scripts/gold_dim_products.sql
@@ -22,6 +22,9 @@ Usage:
 --Using the general principles, update column names using naming conventions
 --Check the quality of the view
 
+IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
+    DROP VIEW gold.dim_products;
+GO
 CREATE VIEW gold.dim_products AS
 SELECT 
 	ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
