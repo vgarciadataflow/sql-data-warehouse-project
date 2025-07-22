2ecbdeac474834e4a4a879c348841fd9e9490daa
vgarciadataflow <vgarcia@dataflowsolutions.co>
2025-07-22 16:11:15 -0500

    Update gold_dim_customers.sql

--- scripts/gold_dim_customers.sql
+++ scripts/gold_dim_customers.sql
@@ -1,3 +1,20 @@
+/*
+======================================================================
+DDL Script: Create Gold View: dim_customers
+======================================================================
+Script Purpose:
+ This script creates views for the Gold layer in the data warehouse.
+ The Gold layer represents the final dimension and fact tables (Star
+ Schema).
+
+ Each view performs transformations and combines data from the Silver
+ layer to produce a clean, enriched, and business-ready dataset.
+
+Usage:
+  -These views can be queried directly for analytics and reporting.
+======================================================================
+*/
+
 --Start from the master table, (cust_info in this case) then use left joins so no data gets left out
 --If you use an inner join and the second table didn't have data on all the customers, you would lose data.
 --As you keep joining, use a query from below to check for duplicates
